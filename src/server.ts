39df9b56e01527365df13465ee244f6686462f2f
farihaa1 <farihaa1403@gmail.com>
2026-06-20 11:51:59 +0600

    modified server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-// import app from "./app";
+import app from "./app";
 import config from "./config";
 
 
@@ -7,9 +7,9 @@ async function server() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    // app.listen(config.port, () => {
-    //   console.log(`Server running on port ${config.port}`);
-    // });
+    app.listen(config.port, () => {
+      console.log(`Server running on port ${config.port}`);
+    });
 
   } catch (error) {
     console.log(error);
